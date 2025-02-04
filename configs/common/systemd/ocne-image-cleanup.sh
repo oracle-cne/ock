35fd491e1681b8234898ca7ab92ec9ce33f168d4
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-02-04 10:06:31 -0600

    Update 2.0 release branch (#22)

--- configs/common/systemd/ocne-image-cleanup.sh
+++ configs/common/systemd/ocne-image-cleanup.sh
@@ -3,11 +3,36 @@
 # Copyright (c) 2024, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 set -e
+set -x
 
 echo "Verifying images"
 REGEX="exporting opaque data as blob.*sha256:([^\\]*).*Can't read link([^\\]*)"
 ERR_FILE="/tmp/podman-corrupt-images.out"
 
+# Clean up tags in the r/w cache that refer to r/o images that
+# no longer exist
+K="container-registry.oracle.com/olcne/kube-proxy"
+C="container-registry.oracle.com/olcne/coredns"
+F='{{.Repository}}:{{.Tag}}'
+ls -l /var/lib/containers/storage
+podman images
+BAD_TAGS=$(podman images 2>&1 1>/dev/null | sed 's/.* of image \([a-z0-9]*\) not found .*/\1/p' | sort | uniq)
+for bad in $BAD_TAGS; do
+  podman rmi -f "$bad"
+done
+KPC=$(podman images --format "$F" --filter "reference=$K:current")
+if [ -z "$KPC" ]; then
+  KP=$(podman images --format "$F" --filter "reference=$K" --sort created | head -1)
+  podman tag "$KP" "$K:current"
+fi
+CDC=$(podman images --format "$F" --filter "reference=$C:current")
+if [ -z "$CDC" ]; then
+  CD=$(podman images --format "$F" --filter "reference=$C" --sort created | head -1)
+  podman tag "$CD" "$C:current"
+fi
+
+# Clean up r/w images that have layers in the r/w cache with lower
+# layers in the r/o cache that no longer exist.
 while true;
 do
   # first check if there are any corrupt images
