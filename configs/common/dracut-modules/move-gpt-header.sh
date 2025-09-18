5f057512b2f227dd334127ca0910b79d81105701
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-09-18 01:11:23 +0000

    Tolerate unformatted disks in initrd

--- configs/common/dracut-modules/move-gpt-header.sh
+++ configs/common/dracut-modules/move-gpt-header.sh
@@ -5,8 +5,15 @@
 set -e
 set -x
 
-ROOT=."/dev/disk/by-label/root"
-DEVICES=$(/usr/sbin/partition-info "${ROOT}")
+# Try to find the device that holds the root filesystem, if one exists.
+# If one does not exist, exit with success.  It is possible that the root
+# partition is not yet available for some reason.
+ROOT="/dev/disk/by-label/root"
+DEVICES=
+if ! DEVICES=$(/usr/sbin/partition-info "${ROOT}"); then
+	echo "Root disk does not exist"
+	exit 0
+fi
 
 read -r -a ROOT_DEVICES <<< "$DEVICES"
 DISK="${ROOT_DEVICES[0]}"
